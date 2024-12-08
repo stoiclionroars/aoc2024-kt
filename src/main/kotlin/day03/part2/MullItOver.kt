b39b74e9529d2403f43e9e2d1eaf142da76258f8
stoiclionroars <lion.stoic@stoiclionroars.dev>
2024-12-08 11:29:06 +0100

    day 03 still working on it.

--- src/main/kotlin/day03/part2/MullItOver.kt
+++ src/main/kotlin/day03/part2/MullItOver.kt
@@ -9,26 +9,25 @@ fun trick(input: String, start: String, end: String, newValue: String): String {
 
 fun main() {
     val input1 = "files/Day03/00_input.txt"
-    val input2= "files/Day03/02_input.txt"
+    val input2 = "files/Day03/02_input.txt"
     val sample1 = "files/Day03/01_sample.txt"
 
-    val lines = File(input2).bufferedReader().readLines()
+    val line = File(input2).readText()
 
     val regex = Regex("""mul\((\d+),\s*(\d+)\)""")
 
     var sum = 0
-    var joinLine = lines.map { it.replace("\n", "") }.joinToString("")
-        var newLine = trick(joinLine, "don", "do", "do")
-        var newLine2 = trick(newLine, "don", "\n", "\n")
+    var newLine= trick(line, "don", "do", "do")
+    var newLine2 = trick(newLine, "don", "\n", "")
 
-        println(newLine2)
-        val matches = regex.findAll(newLine2)
-        val multiplications = matches.map { matchResult ->
-            val (num1, num2) = matchResult.destructured
-            println("${num1.toInt()} ${num2.toInt()}")
-            num1.toInt() * num2.toInt()
-        }.toList().reduce { acc, i -> acc + i }
-        sum += multiplications
+    println(newLine2)
+    val matches = regex.findAll(newLine2)
+    val multiplications = matches.map { matchResult ->
+        val (num1, num2) = matchResult.destructured
+        println("${num1.toInt()} ${num2.toInt()}")
+        num1.toInt() * num2.toInt()
+    }.toList().reduce { acc, i -> acc + i }
+    sum += multiplications
 
     println(sum)
 }
